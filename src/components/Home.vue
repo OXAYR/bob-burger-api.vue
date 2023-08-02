41bd82a5bfe2b4030d5906336d793d3938b4bbcd
OXAYR <uzairmaqsood451@gmail.com>
2023-08-02 13:55:08 +0500

    API & tailwind installed

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import { mapGetters,mapActions } from 'vuex'
+export default {
+  name: 'HomeComp',
+  
+  
+  computed:{
+    ...mapGetters({user: 'getUser'})
+  },
+
+  methods:{
+    ...mapActions({createUser:'registerUser'})
+  }, 
+
+  created() {
+    this.createUser();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
